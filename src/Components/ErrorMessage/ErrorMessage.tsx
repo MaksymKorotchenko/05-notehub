32dbebf135457a19acdecf912a048f99f4b5be11
MaksymKorotchenko <maximuskor29@gmail.com>
2025-08-21 23:24:38 +0300

    Fix folder casing: Components -> components

--- src/Components/ErrorMessage/ErrorMessage.tsx
+++
@@ -1,9 +0,0 @@
-import css from './ErrorMessage.module.css';
-
-export default function ErrorMessage() {
-  return (
-    <strong className={css.text}>
-      Sorry, an error occured while fetching data!
-    </strong>
-  );
-}
