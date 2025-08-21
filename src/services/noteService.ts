e14b6e6c526173a594637906c0b9f6b67943eff2
MaksymKorotchenko <maximuskor29@gmail.com>
2025-08-21 22:42:35 +0300

    hw-5

---
+++ src/services/noteService.ts
@@ -0,0 +1,45 @@
+import axios from 'axios';
+import type { Note, NoteTag } from '../types/note';
+
+axios.defaults.baseURL = 'https://notehub-public.goit.study/api';
+axios.defaults.headers.common['Authorization'] = `Bearer ${
+  import.meta.env.VITE_NOTEHUB_TOKEN
+}`;
+
+interface NotesResponse {
+  notes: Note[];
+  page: number;
+  perPage: number;
+  totalPages: number;
+}
+
+interface NewNote {
+  title: string;
+  content?: string;
+  tag: NoteTag;
+}
+
+export const fetchNotes = async (
+  searchNoteName: string,
+  page: number,
+  perPage: number
+) => {
+  const res = await axios.get<NotesResponse>('/notes', {
+    params: {
+      search: searchNoteName,
+      page,
+      perPage,
+    },
+  });
+  return res.data;
+};
+
+export const deleteNote = async (noteId: string) => {
+  const res = await axios.delete(`/notes/${noteId}`);
+  return res.data;
+};
+
+export const createNote = async (newNoteParams: NewNote) => {
+  const res = await axios.post<NotesResponse>('/notes', newNoteParams);
+  return res.data;
+};
